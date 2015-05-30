f2d8afa24df89f8eaefd56801f3b66ed43b8ad74
Dillon Zhi <dillon.zhi@gmail.com>
2015-05-30 15:45:51 -0700

    myrouter now starts a server that just prints all packets received to the console.

--- myrouter.c
+++ myrouter.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-int main(void)
-{
-    puts("This C program doesn't do anything yet");
+#define BUFFER_SIZE 1024
+
+// Just prints all packets received
+// Send a UDP packet in Bash using
+//      echo -n "your message here" >/dev/udp/localhost/10001
+void print_server_loop(int socket_fd) {
+    while (1) {
+        char buffer[BUFFER_SIZE];
+        struct sockaddr remote_addr;
+        socklen_t remote_addr_len;
+        ssize_t bytes_received = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0,
+                &remote_addr, &remote_addr_len);
+        if (bytes_received < 0) {
+            perror("Error receiving data");
+        } else {
+            printf("Received %d bytes:\n", (int) bytes_received);
+            printf("%.*s\n", (int) bytes_received, buffer);
+        }
+    }
+}
+
+int main(void) {
+    int socket_fd;
+    struct sockaddr_in server_addr;
+    uint16_t port_no = 10001;
+
+    // AF_INET ---> IPv4
+    // SOCK_DGRAM ---> UDP
+    socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket_fd < 0) {
+        perror("Error creating socket");
+        exit(1);
+    }
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(port_no);
+    if (bind(socket_fd,
+            (struct sockaddr *) &server_addr,
+            sizeof(server_addr)) < 0) {
+        perror("Error binding socket");
+        exit(1);
+    }
+
+    print_server_loop(socket_fd);
     return 0;
 }
