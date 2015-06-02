a18965fd458c8fd4fc6eb202919b8535bf824ea1
Dillon Zhi <dillon.zhi@gmail.com>
2015-06-01 17:41:50 -0700

    Clean up the last commit.

--- myrouter.c
+++ myrouter.c
@@ -6,35 +6,37 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 65536
 
 // Just prints all packets received
 // Send a UDP packet in Bash using
-//      echo -n "your message here" >/dev/udp/localhost/10001
-void print_server_loop(int socket_fd) {
-    while (1) {
-        char buffer[BUFFER_SIZE];
-        struct sockaddr_in remote_addr;
-        socklen_t remote_addr_len;
-        ssize_t bytes_received = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0,
-                (struct sockaddr *) &remote_addr, &remote_addr_len);
-        if (bytes_received < 0) {
-            perror("Error receiving data");
-        } else {
-            printf("Received %d bytes ", (int) bytes_received);
-			uint16_t sender_port = ntohs(remote_addr.sin_port);
-			uint32_t sender_ip_addr = ntohl(remote_addr.sin_addr.s_addr);
-			unsigned char ip_bytes[4];
-			ip_bytes[0] = sender_ip_addr & 0xFF;
-			ip_bytes[1] = (sender_ip_addr>>8) & 0xFF;
-			ip_bytes[2] = (sender_ip_addr>>16) & 0xFF;
-			ip_bytes[3] = (sender_ip_addr>>24) & 0xFF;
-			printf("from IP address %u.%u.%u.%u ",
-					ip_bytes[3], ip_bytes[2], ip_bytes[1], ip_bytes[0]);
-			printf("port %u:\n", sender_port);
-            printf("%.*s\n", (int) bytes_received, buffer);
-        }
+//      echo -n "Test" > /dev/udp/localhost/10001
+// Send hexadecimal bytes in Bash using
+//      echo 54657374 | xxd -r -p > /dev/udp/localhost/10001
+void server_loop(int socket_fd) {
+    unsigned char buffer[BUFFER_SIZE];
+    struct sockaddr_in remote_addr;
+    socklen_t remote_addr_len;
+    ssize_t bytes_received = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0,
+            (struct sockaddr *) &remote_addr, &remote_addr_len);
+    if (bytes_received < 0) {
+        perror("Error receiving data");
+        return;
     }
+
+    uint16_t sender_port = ntohs(remote_addr.sin_port);
+    uint32_t sender_ip_addr = ntohl(remote_addr.sin_addr.s_addr);
+    unsigned char ip_bytes[4];
+    ip_bytes[0] = sender_ip_addr & 0xFF;
+    ip_bytes[1] = (sender_ip_addr>>8) & 0xFF;
+    ip_bytes[2] = (sender_ip_addr>>16) & 0xFF;
+    ip_bytes[3] = (sender_ip_addr>>24) & 0xFF;
+
+    printf("Received %d bytes ", (int) bytes_received);
+    printf("from IP address %u.%u.%u.%u ",
+            ip_bytes[3], ip_bytes[2], ip_bytes[1], ip_bytes[0]);
+    printf("port %u:\n", sender_port);
+    printf("%.*s\n", (int) bytes_received, buffer);
 }
 
 int str_to_uint16(const char *str, uint16_t *result) {
@@ -74,11 +76,13 @@ int main(int argc, char **argv) {
     server_addr.sin_port = htons(port_no);
     if (bind(socket_fd,
             (struct sockaddr *) &server_addr,
-            sizeof(server_addr)) < 0) {
+            sizeof server_addr) < 0) {
         perror("Error binding socket");
         exit(1);
     }
 
-    print_server_loop(socket_fd);
+    while (1) {
+        server_loop(socket_fd);
+    }
     return 0;
 }
