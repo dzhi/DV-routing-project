e16d004ca0594ea23001fd421f03b13c368f5e42
Dillon Zhi <dillon.zhi@gmail.com>
2015-06-01 18:30:13 -0700

    Started to define message formats. More printf debugging stuff.

--- myrouter.c
+++ myrouter.c
@@ -8,13 +8,62 @@
 
 #define BUFFER_SIZE 65536
 
-// Just prints all packets received
+enum packet_type {
+    DATA_PACKET = 1,
+    DV_PACKET = 2
+};
+
+struct dv_entry {
+    uint16_t port;
+    uint16_t cost;
+};
+void ntoh_dv_entry (struct dv_entry *n, struct dv_entry *h) {
+    h->port = ntohs(n->port);
+    h->cost = ntohs(n->cost);
+}
+void hton_dv_entry (struct dv_entry *h, struct dv_entry *n) {
+    n->port = htons(h->port);
+    n->cost = htons(h->cost);
+}
+
+// Message format:
+// 1 byte indicating that this is a DV packet
+// Padding to fill the length of a dv_entry
+// 0 or more DV entries
+void handle_dv_packet(uint16_t sender_port,
+        char *buffer, ssize_t bytes_received) {
+    if (bytes_received % sizeof(struct dv_entry) != 0) {
+        printf("Message not understood\n");
+        return;
+    }
+    printf("DV packet from port %u:\n", sender_port);
+    unsigned int i;
+    for (i=sizeof(struct dv_entry); i < bytes_received; i+=sizeof(struct dv_entry)) {
+        struct dv_entry e;
+        ntoh_dv_entry((struct dv_entry *)(buffer+i), &e);
+        printf("Entry: Port %u cost %u\n", e.port, e.cost);
+    }
+}
+
+void print_hexadecimal(char *bytes, int len) {
+    int i;
+    for (i=0; i<len; i++) {
+        if (i!=0) {
+            if (i%16 == 0)
+                printf("\n");
+            else if (i%4 == 0)
+                printf(" ");
+        }
+        printf("%02X", bytes[i]);
+    }
+}
+
 // Send a UDP packet in Bash using
 //      echo -n "Test" > /dev/udp/localhost/10001
 // Send hexadecimal bytes in Bash using
 //      echo 54657374 | xxd -r -p > /dev/udp/localhost/10001
 void server_loop(int socket_fd) {
-    unsigned char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
     struct sockaddr_in remote_addr;
     socklen_t remote_addr_len;
     ssize_t bytes_received = recvfrom(socket_fd, buffer, BUFFER_SIZE, 0,
@@ -36,7 +85,26 @@ void server_loop(int socket_fd) {
     printf("from IP address %u.%u.%u.%u ",
             ip_bytes[3], ip_bytes[2], ip_bytes[1], ip_bytes[0]);
     printf("port %u:\n", sender_port);
-    printf("%.*s\n", (int) bytes_received, buffer);
+    printf("ASCII: %.*s\n", (int) bytes_received, buffer);
+    printf("Hexadecimal:\n");
+    print_hexadecimal(buffer, bytes_received);
+    printf("\n");
+
+    if (bytes_received == 0) {
+        printf("Message not understood\n");
+        return;
+    }
+
+    switch ((unsigned char) buffer[0]) {
+        case DATA_PACKET:
+            printf("Data packet received (behavior not yet implemented)\n");
+        break;
+        case DV_PACKET:
+            handle_dv_packet(sender_port, buffer, bytes_received);
+        break;
+        default:
+            printf("Message not understood\n");
+    }
 }
 
 int str_to_uint16(const char *str, uint16_t *result) {
