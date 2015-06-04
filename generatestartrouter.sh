4e8176346abcbfeabffd9292bc268323569ca71e
lloydxie <swampert_111@yahoo.com>
2015-06-03 22:40:08 -0700

    modified script to test generating a packet

---
+++ generatestartrouter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./myrouter 10000 &
+sleep 0.2
+./myrouter 10001 &
+sleep 0.2
+./myrouter 10002 &
+sleep 0.2
+./myrouter 10003 &
+sleep 0.2
+./myrouter 10004 &
+sleep 0.2
+./myrouter 10005 A D
